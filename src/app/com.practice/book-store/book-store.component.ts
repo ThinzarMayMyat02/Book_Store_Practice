6f17f5796a397e99dedb2c288a31b5fcc7615716
ThinzarMayMyat02 <maymyatthinzar10@gmail.com>
2025-03-09 23:17:08 +0630

    refactor for create and edit form for book

--- src/app/com.practice/book-store/book-store.component.ts
+++ src/app/com.practice/book-store/book-store.component.ts
@@ -1,67 +1,101 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { BookStoreServiceService } from '../service/book-store-service.service';
 
 @Component({
   selector: 'app-book-store',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './book-store.component.html',
-  styleUrl: './book-store.component.scss'
+  styleUrl: './book-store.component.scss',
 })
-export class BookStoreComponent implements OnInit{
+export class BookStoreComponent implements OnInit {
+  form!: FormGroup;
 
-form!:FormGroup;
+  private _formBuilder = inject(FormBuilder);
+  private bookSubject = new BehaviorSubject<Book[]>([]);
 
-private _formBuilder = inject(FormBuilder);
+  private _route = inject(Router);
+  books$ = this.bookSubject.asObservable();
 
-ngOnInit(): void {
-  this.form = this.createBookForm(this.bookFactory());
-  console.log("Form vlaue", this.form.value);
-  this.isAvailableChanged();
-}
+  bookService = inject(BookStoreServiceService);
 
-isAvailableChanged() {
-  this.form.get('isAvailable')?.valueChanges.subscribe((value:boolean) => {
-    console.log('isAvailable value changed', value);
-    if(value){
-      this.form.addControl('quantity', this._formBuilder.control(null, [Validators.required,Validators.min(1)]));
-      this.form.removeControl('restockDate');
-    }else{
-      this.form.addControl('restockDate', this._formBuilder.control(null, [Validators.required]));
-      this.form.removeControl('quantity');
-    }
-  })
-}
+  _activatedRoute = inject(ActivatedRoute);
 
-checkControlContains(controlName: string){
-  return this.form.contains(controlName);
-}
+  ngOnInit(): void {
+    // this.form = this.createBookForm(this.bookFactory());
+    // const isEdit = this.isEdit();
+    // console.log(" Is Edit : ",isEdit);
+    // const id = this.getIdFromRoute();
+    // console.log("Id from Route ",id);
 
-createBookForm(book: Book):FormGroup{
-  return this._formBuilder.group({
-    title: [book.title,[Validators.required]],
-    author: [book.author,[Validators.required]],
-    desc: [book.desc,[Validators.required]],
-    price: [book.price,[Validators.required]],
-    category: [book.category,[Validators.required]],
-    isAvailable: [book.isAvailable,[Validators.required]]
-  });
-}
+    // console.log('Form vlaue', this.form.value);
+    // this.isAvailableChanged();
 
-onSubmit(): void {
-  if(this.form.invalid){
-    this.touchAllFields();
+    const id = this.getIdFromRoute();
+    const book = this.isEdit() ? this.editBook(id): this.bookFactory();
+    this.form = this.createBookForm(book);
+    this.isAvailableChanged();
+    this.changeQuantityControl(book.isAvailable,book.quantity,book.restockDate);
   }
 
-  console.log("Submit Form value", this.form.value);
-}
+  isAvailableChanged() {
+    this.form.get('isAvailable')?.valueChanges.subscribe((value: boolean) => {
+      console.log('isAvailable value changed', value);
+      this.changeQuantityControl(value);
+    });
+  }
 
-touchAllFields(){
-  Object.keys(this.form.controls).forEach(k => {
-    const control = this.form.get(k);
-    control?.markAsTouched();
-    control?.updateValueAndValidity();
-  })
+  changeQuantityControl(isAvailable:boolean|null,
+                        quantity:number|null = null,  // default
+                        restockDate:Date|null = null){
+    if(isAvailable != null){
+      if (isAvailable) {
+        this.form.addControl(
+          'quantity',
+          this._formBuilder.control(quantity, [
+            Validators.required,
+            Validators.min(1),
+          ])
+        );
+        this.form.removeControl('restockDate');
+      } else {
+        this.form.addControl(
+          'restockDate',
+          this._formBuilder.control(restockDate, [Validators.required])
+        );
+        this.form.removeControl('quantity');
+      }
+    }
+  }
+  checkControlContains(controlName: string) {
+    return this.form.contains(controlName);
+  }
+
+  createBookForm(book: Book): FormGroup {
+    return this._formBuilder.group({
+      title: [book.title, [Validators.required]],
+      author: [book.author, [Validators.required]],
+      desc: [book.desc, [Validators.required]],
+      price: [book.price, [Validators.required]],
+      category: [book.category, [Validators.required]],
+      isAvailable: [book.isAvailable, [Validators.required]],
+    });
+  }
+
+  touchAllFields() {
+    Object.keys(this.form.controls).forEach((k) => {
+      const control = this.form.get(k);
+      control?.markAsTouched();
+      control?.updateValueAndValidity();
+    });
   }
 
   bookFactory(): Book {
@@ -73,8 +107,41 @@ touchAllFields(){
       category: null,
       isAvailable: null,
       quantity: null,
-      restockDate: null
+      restockDate: null,
+    };
+  }
+
+  editBook(id: Number) : Book{
+    return this.bookService.findBook(id);
+  }
+
+  getIdFromRoute(): number {
+    var id = +this._activatedRoute.snapshot.params['id'];
+    console.log("Id : "+id);
+    return id;
+  }
+
+  isEdit(){
+    return this._route.url.includes('edit-book');
+  }
+  onSubmit(): void {
+    if (this.form.invalid) {
+      this.touchAllFields();
+    } else {
+      if(this.isEdit()){
+        var id = this.getIdFromRoute();
+        this.bookService.updateBook(id,this.form.value);
+        console.log("Updated book....");
+      }else{
+      const bookConst = this.form.value;
+      this.bookService.addBook(bookConst);
+      console.log("Created book....");
+
+      }
+    this._route.navigate(['/book-table']);
     }
+
+    console.log('Submit Form value', this.form.value);
   }
 }
 
@@ -87,8 +154,8 @@ export const books: Book[] = [
     category: 'Fiction',
     isAvailable: false,
     quantity: null,
-    restockDate: new Date('2021-10-10')
-  }
+    restockDate: new Date('2021-10-10'),
+  },
 ];
 
 export interface Book {
@@ -97,7 +164,7 @@ export interface Book {
   desc: string | null;
   price: number | null;
   category: string | null;
-  isAvailable: boolean|null;
-  quantity: number|null;
-  restockDate?: Date|null;
+  isAvailable: boolean | null;
+  quantity: number | null;
+  restockDate?: Date | null;
 }
